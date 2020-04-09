5df1e3f9f56cbcf6bc58f426cdfe2fc41ed3315b
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-04-09 02:35:47 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User 
+  
+  def initialize
+    @knowledge = []
+  end 
+  
+  def learn(brain_food)
+    @knowledge << brain_food
+  end 
+  
+  def knowledge
+    @knowledge
+  end 
 
 end
